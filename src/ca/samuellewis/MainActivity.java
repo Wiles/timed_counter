04b1cfdd5b0131e6347bb274fd796895137f3e7e
Samuel Lewis <samuelewis@gmail.com>
2013-02-08 15:52:35 -0500

    Start of android annotation integration

--- src/ca/samuellewis/MainActivity.java
+++ src/ca/samuellewis/MainActivity.java
@@ -1,15 +1,33 @@
 package ca.samuellewis;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.NumberPicker;
 
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends Activity {
+	@ViewById
+	NumberPicker np_hours;
+	@ViewById
+	NumberPicker np_minutes;
+	@ViewById
+	NumberPicker np_seconds;
+	
+	@AfterViews
+    public void init() {
+        
+        np_hours.setMinValue(0);
+        np_hours.setMaxValue(99);
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        np_minutes.setMinValue(0);
+        np_minutes.setMaxValue(59);
+
+        np_seconds.setMinValue(0);
+        np_seconds.setMaxValue(59);
     }
 
     @Override
@@ -18,3 +36,4 @@ public class MainActivity extends Activity {
         return true;
     }
 }
+	
