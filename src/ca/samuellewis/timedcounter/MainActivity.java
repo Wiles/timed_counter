4f7e4de0b4d28eeba6f65194f1e6955e6386b476
Samuel Lewis <samuelewis@gmail.com>
2013-02-14 19:44:24 -0500

    New working project

---
+++ src/ca/samuellewis/timedcounter/MainActivity.java
@@ -0,0 +1,11 @@
+package ca.samuellewis.timedcounter;
+
+import android.app.Activity;
+
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends Activity {
+
+
+}
